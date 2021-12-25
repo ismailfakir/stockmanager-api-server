9fcd8fe5dfedda41409f3d1119367981bdaa786a
ismail <md.ismail.fakir@gmail.com>
2021-12-25 01:09:05 +0100

    create env file bash script updated

--- .github/scripts/create_dot_env.sh
+++ .github/scripts/create_dot_env.sh
@@ -4,7 +4,7 @@
 touch .env
 # add the env values to .env file
 echo "DB_HOST=$DB_HOST" >> .env
-#echo DB_PORT=${ secrets.DB_PORT } >> .env
-#echo DB_DATABASE=${ secrets.DB_DATABASE } >> .env
-#echo DB_USER=${ secrets.DB_USER } >> .env
-#echo DB_PASS=${ secrets.DB_PASS } >> .env
+echo "DB_PORT=$DB_PORT" >> .env
+echo "DB_DATABASE=$DB_DATABASE" >> .env
+echo "DB_USER=$DB_USER" >> .env
+echo "DB_PASS=$DB_PASS" >> .env
