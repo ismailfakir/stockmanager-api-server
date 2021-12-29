9c90a00d0063f61c9ff231b4da7e5d8abc3e13fc
ismail <md.ismail.fakir@gmail.com>
2021-12-29 01:28:06 +0100

    welcome message updated

--- server.js
+++ server.js
@@ -41,10 +41,14 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' '+time;
   res.json(
     { 
       message: "Welcome to stockmanager rest api server.",
-      time: '${Date.now()}'
+      time: dateTime
     }
   );
 });
